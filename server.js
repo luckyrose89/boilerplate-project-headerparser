b98f85ab109dbc0da2eb10a3c09e6176437a83fd
avidrutham <divyamathur89@gmail.com>
2018-08-07 18:15:27 +0530

    changed server.js syntax for es6 integration

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -14,19 +16,19 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
